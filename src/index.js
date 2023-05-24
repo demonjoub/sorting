9bfc313502795b771645dcf4d9e939142aeec968
todsapoly <todsapol.yod@ascendcorp.com>
2023-05-24 15:15:01 +0700

    feat: check and gen cid

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { generateCid, verifyCid } from './utils/cid';
 import { bubbleSort, sort, insertionSort, mergeSort } from './utils/sort';
 
 console.time('recursive sorting');
@@ -15,3 +16,10 @@ console.log('\n');
 console.time('mergeSort');
 console.log(mergeSort([7, 12, 1, 2, 6, 3, 4, 5, 2]));
 console.timeEnd('mergeSort');
+console.log('\n');
+console.log('Check cid: 1319001913429', verifyCid(1319001913429));
+const digitalNumber = generateCid();
+console.log(
+  `Generate cid: ${digitalNumber} check cid:`,
+  verifyCid(digitalNumber)
+);
---
+++ src/utils/cid.js
@@ -0,0 +1,36 @@
+export const verifyCid = (cid) => {
+  if (!cid) return false;
+  let idCardNumber = cid;
+  if (typeof cid === 'number') {
+    idCardNumber = idCardNumber.toString();
+  }
+  const cidArr = idCardNumber.toString().split('');
+  const regex = /^[0-9]\d*$/;
+  if (idCardNumber.match(regex) == null) {
+    return false;
+  }
+  if (cidArr.length < 1 || cidArr.length != 13) {
+    return false;
+  }
+  let sum = 0;
+  for (let i = 0; i < cidArr.length - 1; i++) {
+    sum += parseInt(cidArr[i]) * (13 - i);
+  }
+  const checkDigit = (11 - (sum % 11)) % 10;
+  if (cidArr[cidArr.length - 1] == checkDigit) {
+    return true;
+  }
+  return false;
+};
+
+export const generateCid = () => {
+  let result = '';
+  let sum = 0;
+  for (let i = 0; i < 12; i++) {
+    const digitalNumber = Math.floor(Math.random(9) * 10);
+    result += digitalNumber;
+    sum += parseInt(digitalNumber) * (13 - i);
+  }
+  const lastDigit = (11 - (sum % 11)) % 10;
+  return `${result}${lastDigit}`;
+}
